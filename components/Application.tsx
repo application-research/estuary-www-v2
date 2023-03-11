c76fe118ba25194222004c9089ba678acde9bcbb
@wwwjim <send@jimmy.email>
2023-03-10 18:02:45 -0800

    updated pricing

--- components/Application.tsx
+++ components/Application.tsx
@@ -12,6 +12,26 @@ import * as Filecoin from '@common/filecoin';
 import PackageJSON from '@root/package.json';
 import DefaultLayout from '@components/DefaultLayout';
 
+const Selection = (props) => {
+  return (
+    <div className={styles.selection}>
+      <div className={styles.radio} />
+      <div className={styles.left}>{props.label}</div>
+      <div className={styles.right}>
+        {props.five}
+        {props.four}
+        {props.three}
+        {props.two}
+        {props.one}
+      </div>
+    </div>
+  );
+};
+
+const Stat = (props) => {
+  return <span className={styles.stat} {...props} />;
+};
+
 export default function Application(props) {
   return (
     <DefaultLayout>
@@ -27,46 +47,67 @@ export default function Application(props) {
         </h2>
         <p className={styles.p}>
           A fully managed Filecoin storage deal maker for getting your data sealed on the Filecoin Network with storage providers of your choice. Import your Filecoin wallet or
-          have us generate a new one for you. You are responsible for getting your own Filecoin+ Datacap.
+          have us generate a new one for you. You are responsible for getting your own Filecoin+ Datacap. Delta nodes also take out-of-band data flow so you do not necessarily need
+          to utelize the upload bandwith.
         </p>
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Basic</div>
-          <div className={styles.right}>
-            <span className={styles.stat}>
-              2 GB <strong>Ram</strong>
-            </span>
-            <span className={styles.stat}>
-              1 <strong>CPU</strong>
-            </span>
-            <span className={styles.stat}>
-              20 TB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Basic"
+          one={
+            <Stat>
               10 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              20 TB <strong>Storage</strong>
+            </Stat>
+          }
+          three={
+            <Stat>
+              250 MBPS <strong>Up</strong>
+            </Stat>
+          }
+          four={
+            <Stat>
+              2 GB <strong>Ram</strong>
+            </Stat>
+          }
+          five={
+            <Stat>
+              1 <strong>CPU</strong>
+            </Stat>
+          }
+        />
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Heavy</div>
-          <div className={styles.right}>
-            <span className={styles.stat}>
-              64 GB <strong>Ram</strong>
-            </span>
-            <span className={styles.stat}>
-              8 <strong>CPUs</strong>
-            </span>
-            <span className={styles.stat}>
-              100 TB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Heavy"
+          one={
+            <Stat>
               100 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              100 TB <strong>Storage</strong>
+            </Stat>
+          }
+          three={
+            <Stat>
+              1 GBPS <strong>Up</strong>
+            </Stat>
+          }
+          four={
+            <Stat>
+              64 GB <strong>Ram</strong>
+            </Stat>
+          }
+          five={
+            <Stat>
+              8 <strong>CPU</strong>
+            </Stat>
+          }
+        />
 
         <h2 className={styles.h2}>Edge Node</h2>
         <p className={styles.p}>
@@ -74,132 +115,174 @@ export default function Application(props) {
           for you data, or making it available to an AI model. Storage providers will appreciate you for paying for the unsealed copies and helping them maximize their profit.
         </p>
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Pond</div>
-          <div className={styles.right}>
-            <span className={styles.stat}>
-              4 GB <strong>Ram</strong>
-            </span>
-            <span className={styles.stat}>
-              2 <strong>CPUs</strong>
-            </span>
-            <span className={styles.stat}>
-              10 TB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Pond"
+          one={
+            <Stat>
               10 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              10 TB <strong>Storage</strong>
+            </Stat>
+          }
+          three={
+            <Stat>
+              250 MBPS <strong>Down</strong>
+            </Stat>
+          }
+          four={
+            <Stat>
+              4 GB <strong>Ram</strong>
+            </Stat>
+          }
+          five={
+            <Stat>
+              2 <strong>CPU</strong>
+            </Stat>
+          }
+        />
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Stream</div>
-          <div className={styles.right}>
-            <span className={styles.stat}>
-              32 GB <strong>Ram</strong>
-            </span>
-            <span className={styles.stat}>
-              4 <strong>CPUs</strong>
-            </span>
-            <span className={styles.stat}>
-              50 TB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Stream"
+          one={
+            <Stat>
               50 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              50 TB <strong>Storage</strong>
+            </Stat>
+          }
+          three={
+            <Stat>
+              1 GBPS <strong>Down</strong>
+            </Stat>
+          }
+          four={
+            <Stat>
+              32 GB <strong>Ram</strong>
+            </Stat>
+          }
+          five={
+            <Stat>
+              4 <strong>CPU</strong>
+            </Stat>
+          }
+        />
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Lake</div>
-          <div className={styles.right}>
-            <span className={styles.stat}>
-              64 GB <strong>Ram</strong>
-            </span>
-            <span className={styles.stat}>
-              8 <strong>CPUs</strong>
-            </span>
-            <span className={styles.stat}>
-              100 TB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Lake"
+          one={
+            <Stat>
               100 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              100 TB <strong>Storage</strong>
+            </Stat>
+          }
+          three={
+            <Stat>
+              1 GBPS <strong>Down</strong>
+            </Stat>
+          }
+          four={
+            <Stat>
+              64 GB <strong>Ram</strong>
+            </Stat>
+          }
+          five={
+            <Stat>
+              8 <strong>CPU</strong>
+            </Stat>
+          }
+        />
 
         <h2 className={styles.h2}>Data Node</h2>
         <p className={styles.p}>A fully managed storage capacity add-on, providing your account with more storage capacity.</p>
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Trunk</div>
-          <div className={styles.right}>
-            <span className={styles.stat} />
-            <span className={styles.stat} />
-            <span className={styles.stat}>
-              100 TB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Trunk"
+          one={
+            <Stat>
               25 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              100 TB <strong>Storage</strong>
+            </Stat>
+          }
+          three={<Stat />}
+          four={<Stat />}
+          five={<Stat />}
+        />
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Chest</div>
-          <div className={styles.right}>
-            <span className={styles.stat} />
-            <span className={styles.stat} />
-            <span className={styles.stat}>
-              1 PB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Chest"
+          one={
+            <Stat>
               240 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              1 PB <strong>Storage</strong>
+            </Stat>
+          }
+          three={<Stat />}
+          four={<Stat />}
+          five={<Stat />}
+        />
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Vault</div>
-          <div className={styles.right}>
-            <span className={styles.stat} />
-            <span className={styles.stat} />
-            <span className={styles.stat}>
-              5 PB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Vault"
+          one={
+            <Stat>
               1200 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              5 PB <strong>Storage</strong>
+            </Stat>
+          }
+          three={<Stat />}
+          four={<Stat />}
+          five={<Stat />}
+        />
 
         <h2 className={styles.h2}>Postgres</h2>
         <p className={styles.p}>A fully managed hosted PostgreSQL with external connectivity.</p>
 
-        <div className={styles.selection}>
-          <div className={styles.radio} />
-          <div className={styles.left}>Default</div>
-          <div className={styles.right}>
-            <span className={styles.stat}>
-              4 GB <strong>Ram</strong>
-            </span>
-            <span className={styles.stat}>
-              2 <strong>CPUs</strong>
-            </span>
-            <span className={styles.stat}>
-              128 GB <strong>Storage</strong>
-            </span>
-            <span className={styles.stat}>
+        <Selection
+          label="Default"
+          one={
+            <Stat>
               20 <strong>FIL / month</strong>
-            </span>
-          </div>
-        </div>
+            </Stat>
+          }
+          two={
+            <Stat>
+              128 GB <strong>Storage</strong>
+            </Stat>
+          }
+          three={<Stat />}
+          four={
+            <Stat>
+              4 GB <strong>RAM</strong>
+            </Stat>
+          }
+          five={
+            <Stat>
+              2 <strong>CPUs</strong>
+            </Stat>
+          }
+        />
       </div>
     </DefaultLayout>
   );
